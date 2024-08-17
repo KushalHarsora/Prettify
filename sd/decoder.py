6dcebb575ed01ed838a04dc898061d6293d8642a
Kushal Harsora <kushalharsora13@gmail.com>
2024-08-17 14:04:51 +0530

    Decoder Done

--- sd/decoder.py
+++ sd/decoder.py
@@ -3,6 +3,39 @@ from torch import nn
 from torch.nn import functional as F
 from attention import SelfAttention
 
+class VAE_AttentionBlock(nn.Module):
+
+    def __init__(self, channels: int):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_size, features, height, width)
+
+        residue = x
+
+        n, c, h, w = x.shape
+
+        # (Batch_size, features, height, width) -> (Batch_size, features, height * width)        
+        x = x.view(n, c, h * w)
+
+        # (Batch_size, features, height * width) -> (Batch_size, height * width, features)        
+        x = x.transpose(-1, -2)
+
+        # (Batch_size, height * width, features) -> (Batch_size, height * width, features)
+        x = self.attention(x)
+
+        # (Batch_size, height * width, features) -> (Batch_size, features, height * width)
+        x = x.transpose(-1, -2)
+
+        # (Batch_size, features, height * width) -> (Batch_size, features, height, width)
+        x = x.view((n, c, h, w))
+
+        x += residue
+
+        return x
+
 class VAE_ResidualBlock(nn.Module):
 
     def __init__(self, in_channels, out_channels):
