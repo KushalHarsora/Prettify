4483f74dccbb46f0fc0ab6b045c07cc27e0d4468
Kushal Harsora <kushalharsora13@gmail.com>
2024-08-17 14:40:04 +0530

    Decoder Complete

--- sd/decoder.py
+++ sd/decoder.py
@@ -70,3 +70,72 @@ class VAE_ResidualBlock(nn.Module):
         x = self.conv_2(x)
 
         return x + self.residual_layer(residue)
+
+class VAE_Decorder(nn.Sequential):
+
+    def __init__(self):
+        super().__init__(
+            nn.Conv2d(4, 4, kernel_size=1, padding=0),
+
+            nn.Conv2d(4, 512, kernel_size=3, padding=0),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_AttentionBlock(512),
+
+            VAE_ResidualBlock(512, 512),
+
+            VAE_ResidualBlock(512, 512),
+            
+            VAE_ResidualBlock(512, 512),
+
+            # (Batch_size, 512, Height / 8, Width / 8) -> (Batch_size, 512, Height / 8, Width / 8)
+            VAE_ResidualBlock(512, 512),
+
+            # upscaling the matrix twise the original => (Batch_size, 512, Height / 8, Width / 8) -> (Batch_size, 512, Height / 4, Width / 4)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+            VAE_ResidualBlock(512, 512),
+
+            # upscaling the matrix twise the original => (Batch_size, 512, Height / 4, Width / 4) -> (Batch_size, 512, Height / 2, Width / 2)
+            nn.Upsample(scale_factor=2),
+
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+
+            VAE_ResidualBlock(512, 256),
+            VAE_ResidualBlock(256, 256),
+            VAE_ResidualBlock(256, 256),
+
+            # upscaling the matrix twise the original => (Batch_size, 256, Height / 2, Width / 2) -> (Batch_size, 256, Height, Width)
+            nn.Upsample(scale_factor=2), 
+
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),  
+
+            VAE_ResidualBlock(256, 128),
+            VAE_ResidualBlock(128, 128),
+            VAE_ResidualBlock(128, 128),   
+
+            nn.GroupNorm(32, 128),
+
+            nn.SiLU(),
+
+            # Convert features into rgb values => (Batch_size, 128, height, width) -> (Batch_size, 3, height, width)
+            nn.Conv2d(128, 3, kernel_size=3, padding=1)     
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_size, 4, Height / 8, Width / 8)
+
+        # reduce scaling
+        x /= 0.18215
+
+        for module in self:
+            x = module(x)
+
+        # (Batch_size, 3, height, width)
+        return x
+        
